c66a4749cfbbe0894840f7922463140ef7c4a468
Cisne Parra <adparra2@utpl.edu.ec>
2023-12-07 16:32:23 -0500

    cambios de la clase

--- problematicas/problema3/src/problema3/Problema3.java
+++ problematicas/problema3/src/problema3/Problema3.java
@@ -14,7 +14,13 @@ public class Problema3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String acumulador = "";
+        for (int fh = 20; fh <= 100; fh += 4) {
+            double ct = 0;
+            ct = ct + 5 / 9.0 * (fh - 32);
+            acumulador = String.format("%s%.2f\n",acumulador,ct);
+        }
+        System.out.printf("%s", acumulador);
     }
-    
+
 }
